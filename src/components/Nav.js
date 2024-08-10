64fc3be3de9041175a083b7bb4c71f6bb2a3f18b
Bach Linh Vo <linh06102004@gmail.com>
2024-08-09 21:32:15 -0700

    Added an authModal and decorated the home page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,31 @@
 import logo from '../images/logo_transparent.png'
 import colorLogo from '../images/logo.png'
 
-const Nav = ({ minimal, authToken}) => {
+const Nav = ({ authToken, minimal, setShowModal, showModal, setIsSignUp }) => {
+    const handleClick = () => {
+        setShowModal(true);
+        setIsSignUp(false);
+    };
 
     return (
         <nav>
-        <div className="logo-container">
-            <img className="logo" src={minimal ? colorLogo: logo}/>
-        </div>
-
-        {!authToken && <button className='nav-button'>Log in</button>}
+            <div className="logo-container">
+                <img
+                    className="logo"
+                    src={minimal ? colorLogo : logo}
+                    alt="logo"
+                />
+            </div>
+            {!authToken && !minimal && (
+                <button
+                    className="nav-button"
+                    onClick={handleClick}
+                    disabled={showModal}
+                >
+                    Log in
+                </button>
+            )}
         </nav>
-    )
-}
-export default Nav
+    );
+};
+export default Nav;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,46 @@
 import Nav from '../components/Nav'
+import AuthModal from "../components/AuthModal"
+import {useState} from 'react'
+import {useCookies} from "react-cookie"
 
 const Home = () => {
-
-    const authToken = false
+    const [showModal, setShowModal] = useState(false)
+    const [isSignUp, setIsSignUp] = useState(true)
+    const [cookies, setCookie, removeCookie] = useCookies(['user'])
+    const authToken = cookies.AuthToken
 
     const handleClick = () => {
-        console.log("Clicked")
+        if (authToken) {
+            removeCookie('UserId', cookies.UserId)
+            removeCookie('AuthToken', cookies.AuthToken)
+            window.location.reload()
+            return
+        }
+        setShowModal(true)
+        setIsSignUp(true)
     }
 
     return (
-        <>
-        <Nav minimal={false} authToken={authToken}/>
-        <div className={"home"}>
-            <h1>Swipe left to Connect</h1>
-            <button className="primary-button" onClick={handleClick}>
-                {authToken ? 'Signout' : 'Create Account'}
-            </button>
+        <div className="overlay">
+            <Nav
+                authToken={authToken}
+                minimal={false}
+                setShowModal={setShowModal}
+                showModal={showModal}
+                setIsSignUp={setIsSignUp}
+            />
+            <div className="home">
+                <h1 className="primary-title">Swipe Left to Connect</h1>
+                <button className="primary-button" onClick={handleClick}>
+                    {authToken ? 'Signout' : 'Create Account'}
+                </button>
+
+
+                {showModal && (
+                    <AuthModal setShowModal={setShowModal} isSignUp={isSignUp}/>
+                )}
+            </div>
         </div>
-        </>
     )
 }
 export default Home
